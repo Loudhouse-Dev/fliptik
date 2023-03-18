2bd5c4664307bc9703963d962c19edda232c2972
John <contactme@johnnyhowell.dev>
2023-03-18 12:51:02 -0500

    Boilerplate Finished

--- fliptik/src/pages/_app.tsx
+++ fliptik/src/pages/_app.tsx
@@ -1,11 +1,36 @@
 import { type AppType } from "next/app";
-
+import type { AppProps } from "next/app";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { useRouter } from "next/router";
 import { api } from "lib/utils/api";
 
 import "lib/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  return (
+    <ClerkProvider {...pageProps}>
+      {publicPages.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
