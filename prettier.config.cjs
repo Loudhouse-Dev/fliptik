a8b8a0dc42545c8ee1565896d1a0afbe787dcf64
John <contactme@johnnyhowell.dev>
2023-03-18 15:30:15 -0500

    New Config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
 };
-
-module.exports = config;
